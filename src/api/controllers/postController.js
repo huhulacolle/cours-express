e86b0972ed7c267d8d275383c83726d6600524c2
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-11-08 16:40:56 +0100

    j'ai le truc du prof

--- src/api/controllers/postController.js
+++ src/api/controllers/postController.js
@@ -21,41 +21,56 @@ exports.createAPost = (req, res) => {
         if (error) {
             res.status(401);
             console.log(error);
-            res.json({ message: "Requete invalide." });
+            res.json({ message: "Reqûete invalide." });
         }
         else {
             res.status(201);
-            res.json({post});
+            res.json(post);
         }
     })
 }
 
-exports.getAPost = async (req, res) => {
-    try {
-        const result = await Post.findById({_id: req.params.post_id}).exec();
-        if (result == null) {
-            return res.status(400).send();    
+exports.getAPost = (req, res) => {
+    Post.findById(req.params.post_id, (error, post) => {
+        if (error) {
+            res.status(401);
+            console.log(error);
+            res.json({ message: "Reqûete invalide." });
         }
-        return res.json(result);
-    } catch (error) {
-        return res.status(400).send("Erreur")
-    }
+        else {
+            res.status(200);
+            res.json(post);
+        }
+
+    })
 }
 
-exports.updateAPost = async (req, res) => {
-    try {
-        await Post.findByIdAndUpdate({_id: req.params.post_id}, {title: req.body.title, content: req.body.content}).exec();
-        return res.status(201).send();
-    } catch (error) {
-        return res.status(400).send("Erreur");
-    }
+exports.updateAPost = (req, res) => {
+    Post.findByIdAndUpdate(req.params.post_id, req.body, { new: true }, (error, post) => {
+        if (error) {
+            res.status(401);
+            console.log(error);
+            res.json({ message: "Reqûete invalide." });
+        }
+        else {
+            res.status(200);
+            res.json(post);
+        }
+
+    })
 }
 
-exports.deleteApost = async (req, res) => {
-    try {
-        await Post.findOneAndDelete({_id: req.params.post_id}).exec();
-        return res.send();
-    } catch (error) {
-        return res.status(400).send("Erreur");
-    }
+exports.deleteApost = (req, res) => {
+    Post.findByIdAndRemove(req.params.post_id, (error) => {
+        if (error) {
+            res.status(401);
+            console.log(error);
+            res.json({ message: "Reqûete invalide." });
+        }
+        else {
+            res.status(200);
+            res.json({message: "Article supprimé"});
+        }
+
+    })
 }
